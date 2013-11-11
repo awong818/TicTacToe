ca12513df664dfcd32389062f6e1b53907d000a6
Roy Xia <roy062@gmail.com>
2013-11-10 16:39:17 -0800

    Added touch input to test game board. More or less ready to integrate with actual gameplay.

--- src/com/example/tictactoe/TicTacToeBoard.java
+++ src/com/example/tictactoe/TicTacToeBoard.java
@@ -5,19 +5,34 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class TicTacToeBoard extends View {
-	private Paint mBitmapPaint;
+public class TicTacToeBoard extends View{
+	private Paint mBitmapPaint, mHighlightPaint, mLinePaint;
 	private int tileSize;
 	private int xWhitespace, yWhitespace;
 	private Bitmap xMark, oMark;
+	private int[][] boardData;
+	private int curActionPointer = -1;
+	private Rect drawingRect;
+	private int cursorXPos, cursorYPos;
 	
 	public TicTacToeBoard(Context context, AttributeSet attributes)
 	{
 		super(context, attributes);
 		mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		mHighlightPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		mHighlightPaint.setARGB(127, 255, 255, 0);
+		mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		mLinePaint.setARGB(255, 0, 0, 0);
+		boardData = new int[9][9];
+		drawingRect = new Rect(0, 0, 0, 0);
+		cursorXPos = -1;
+		cursorYPos = -1;
 	}
 	
 	protected void onSizeChanged(int w, int h, int oldw, int oldh)
@@ -60,10 +75,66 @@ public class TicTacToeBoard extends View {
 	
 	protected void onDraw(Canvas canvas)
 	{
+		for (int i = 1; i < 9; i++)
+		{
+			canvas.drawLine((float)xWhitespace + i * tileSize, (float)yWhitespace, (float)xWhitespace + i * tileSize, (float)yWhitespace + tileSize * 9, mLinePaint);
+			canvas.drawLine((float)xWhitespace, (float)yWhitespace + i * tileSize, (float)xWhitespace + tileSize * 9, (float)yWhitespace + i * tileSize, mLinePaint);
+		}
+		drawingRect.top = yWhitespace;
+		drawingRect.bottom = yWhitespace + tileSize;
 		for (int i = 0; i < 9; i++)
 		{
-			canvas.drawBitmap(xMark, xWhitespace + i * tileSize, yWhitespace, mBitmapPaint);
-			canvas.drawBitmap(oMark, xWhitespace + i * tileSize, yWhitespace + tileSize, mBitmapPaint);
+			drawingRect.left = xWhitespace;
+			drawingRect.right = xWhitespace + tileSize;
+			for (int j = 0; j < 9; j++)
+			{
+				if (boardData[j][i] == 1)
+					canvas.drawBitmap(xMark, null, drawingRect, mBitmapPaint);
+				else if (boardData[j][i] == 2)
+					canvas.drawBitmap(oMark, null, drawingRect, mBitmapPaint);
+				drawingRect.left += tileSize;
+				drawingRect.right += tileSize;
+			}
+			drawingRect.top += tileSize;
+			drawingRect.bottom += tileSize;
 		}
+		if (cursorXPos >= 0 && cursorXPos < 9 && cursorYPos >= 0 && cursorYPos < 9)
+		{
+			drawingRect.left = xWhitespace + cursorXPos * tileSize;
+			drawingRect.top = yWhitespace + cursorYPos * tileSize;
+			drawingRect.right = xWhitespace + (cursorXPos + 1) * tileSize;
+			drawingRect.bottom = yWhitespace + (cursorYPos + 1) * tileSize;
+			canvas.drawRect(drawingRect, mHighlightPaint);
+		}
+	}
+	
+	public boolean onTouchEvent(MotionEvent e)
+	{
+		if (curActionPointer == -1 && e.getActionMasked() == MotionEvent.ACTION_DOWN)
+			curActionPointer = e.getPointerId(0);
+		if (curActionPointer == e.getPointerId(0))
+		{
+			translateToBoardPosition((int)e.getX(), (int)e.getY());
+			if (e.getActionMasked() == MotionEvent.ACTION_UP)
+			{
+				if (cursorXPos >= 0 && cursorXPos < 9 && cursorYPos >= 0 && cursorYPos < 9)
+				{
+					boardData[cursorXPos][cursorYPos] += 1;
+					boardData[cursorXPos][cursorYPos] %= 3;
+				}
+				cursorXPos = -1;
+				cursorYPos = -1;
+				curActionPointer = -1;
+			}
+			invalidate();
+		}
+		super.onTouchEvent(e);
+		return true;
+	}
+	
+	private void translateToBoardPosition(int x, int y)
+	{
+		cursorXPos = (x - xWhitespace) / tileSize;
+		cursorYPos = (y - yWhitespace) / tileSize;
 	}
 }
